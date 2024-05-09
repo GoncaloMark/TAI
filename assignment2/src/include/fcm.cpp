c9032400cb42706db8ebbbd054763190db7921c2
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2024-05-09 13:08:35 +0100

    move fcm source to include folder

--- assignment2/src/include/fcm.cpp
+++ assignment2/src/include/fcm.cpp
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream:assignment2/src/fcm.cpp
 #include "include/utils/fcm.hpp"
+=======
+#include "fcm.hpp"
+>>>>>>> Stashed changes:assignment2/src/include/fcm.cpp
 
 namespace FCM {
 
