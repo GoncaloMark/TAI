780d954112264e250735c417b917390b0c4e4fe4
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2024-05-31 10:38:30 +0100

    Update GetMaxFreqs.cpp

--- assignment3/GetMaxFreqs/src/GetMaxFreqs.cpp
+++ assignment3/GetMaxFreqs/src/GetMaxFreqs.cpp
@@ -71,11 +71,53 @@ void helpMessage() {
     cerr << "                   AudioFile" << endl;
 }
 
+// Function to compute the FFT signature
+void computeFFTSig(const vector<short>& samples, int nFrames, int ws, int sh, int ds, int nf, ofstream& os) {
+    fftw_complex in[ws] = {};
+    fftw_complex out[ws] = {};
+
+    fftw_plan plan;
+    double power[ws/2];
+    plan = fftw_plan_dft_1d(ws, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    for(int n = 0 ; n <= (nFrames - ws * ds) / (sh * ds) ; ++n) {
+
+        for(int k = 0 ; k < ws ; ++k) { // Convert to mono and down-sample
+            in[k][0] = (int)samples[(n * (sh * ds) + k * ds) << 1] + samples[((n * (sh * ds) + k * ds) << 1) + 1];
+            for(int l = 1 ; l < ds ; ++l) {
+                in[k][0] += (int) samples[(n * (sh * ds) + k * ds + l) << 1] + samples[((n * (sh * ds) + k * ds + l) << 1) + 1];
+            }
+
+        }
+
+        fftw_execute(plan);
+
+        for(int k = 0 ; k < ws/2 ; ++k) {
+            power[k] = out[k][0] * out[k][0] + out[k][1] * out[k][1];
+        }
+
+        unsigned maxPowerIdx[ws/2];
+        for(int k = 0 ; k < ws/2 ; ++k) {
+            maxPowerIdx[k] = k;
+        }
+
+        partial_sort(maxPowerIdx, maxPowerIdx + nf, maxPowerIdx + ws/2,[&power](int i, int j) { return power[i] > power[j]; });
+
+        if(os) {
+            for(int i = 0 ; i < nf ; ++i) {
+                // To store in a byte, truncate to a max of 255
+                os.put(maxPowerIdx[i] > 255 ? 255 : maxPowerIdx[i]);
+            }
+        }
+    }
+    fftw_destroy_plan(plan);
+}
+
 int main (int argc, char* argv[]) {
 
     // Declare variables
 	bool verbose { false };
-	char* oFName = nullptr;
+    string oFName;
     string iFName;
 	ofstream os;
 	int ws { WS };
@@ -109,68 +151,26 @@ int main (int argc, char* argv[]) {
     // Start Program execution
 	SndfileHandle audioFile = createAudioFile(iFName);
 
-	if(oFName != nullptr) {
-		os.open(oFName, ofstream::binary);
-		if(!os) {
-			cerr << "Warning: failed to open file to write\n";
-		}
-	}
-
     if(verbose) {
         printf("Sample rate : %d\n",  audioFile.samplerate());
         printf("Channels    : %d\n",  audioFile.channels());
         printf("Frames      : %ld\n", (long int)audioFile.frames());
     }
 
+	if(!oFName.empty()) {
+		os.open(oFName, ofstream::binary);
+		if(!os) {
+			cerr << "Warning: failed to open file to write\n";
+		}
+	}
+
     // Read audio samples
     vector<short> samples(audioFile.frames() * audioFile.channels());
     audioFile.readf(samples.data(), audioFile.frames());
-    //short* samples = new short[audioFile.frames() * audioFile.channels()];
-	//audioFile.readf(samples, audioFile.frames());
+    int nFrames = static_cast<int>(audioFile.frames());
 
-    fftw_complex in[ws] = {};
-    fftw_complex out[ws] = {};
+    computeFFTSig(samples, nFrames, ws, sh, ds, nf, os);
 
-	fftw_plan plan;
-	double power[ws/2];
-	plan = fftw_plan_dft_1d(ws, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-
-	for(int n = 0 ; n <= (audioFile.frames() - ws * ds) / (sh * ds) ; ++n) {
-
-		for(int k = 0 ; k < ws ; ++k) { // Convert to mono and down-sample
-			in[k][0] = (int)samples[(n * (sh * ds) + k * ds) << 1] + samples[((n * (sh * ds) + k * ds) << 1) + 1];
-			for(int l = 1 ; l < ds ; ++l) {
-				in[k][0] += (int) samples[(n * (sh * ds) + k * ds + l) << 1] + samples[((n * (sh * ds) + k * ds + l) << 1) + 1];
-			}
-
-		}
-
-		fftw_execute(plan);
-
-		for(int k = 0 ; k < ws/2 ; ++k) {
-            power[k] = out[k][0] * out[k][0] + out[k][1] * out[k][1];
-        }
-
-		unsigned maxPowerIdx[ws/2];
-		for(int k = 0 ; k < ws/2 ; ++k) {
-            maxPowerIdx[k] = k;
-        }
-
-		partial_sort(maxPowerIdx, maxPowerIdx + nf, maxPowerIdx + ws/2,[&power](int i, int j) { return power[i] > power[j]; });
-
-		if(os) {
-			for(int i = 0 ; i < nf ; ++i) {
-				// To store in a byte, truncate to a max of 255
-				os.put(maxPowerIdx[i] > 255 ? 255 : maxPowerIdx[i]);
-			}
-
-		}
-
-	}
-
-    //delete[] samples;
-
-	fftw_destroy_plan(plan);
 	return EXIT_SUCCESS;
 }
 
