ef3d48257e4a43318098e777f82366bec524e48e
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2024-05-31 14:54:51 +0100

    d

---
+++ assignment3/src/include/Helpers.cpp
@@ -0,0 +1,8 @@
+//
+// Created by renan on 31/05/24.
+//
+
+#include "Helpers.hpp"
+
+namespace UTILS {
+} // UTILS
---
+++ assignment3/src/include/Helpers.hpp
@@ -0,0 +1,16 @@
+//
+// Created by renan on 31/05/24.
+//
+
+#ifndef TAI_P3_HELPERS_HPP
+#define TAI_P3_HELPERS_HPP
+
+namespace UTILS {
+
+    class Helpers {
+
+    };
+
+} // UTILS
+
+#endif //TAI_P3_HELPERS_HPP
