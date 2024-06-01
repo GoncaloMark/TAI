3b937513d683dfc93923a0fb89ccd2113634c2c8
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2024-06-01 15:38:58 +0100

    Update Helpers.cpp

--- assignment3/src/Helpers.cpp
+++ assignment3/src/Helpers.cpp
@@ -22,18 +22,19 @@ namespace UTILS {
             std::cerr << "Error: invalid audio file\n";
             exit(EXIT_FAILURE);
         }
-        if (audioFile.channels() != 2) {
-            std::cerr << "Error: currently supports only 2 channels\n";
+        if (audioFile.channels() <= 0) {
+            std::cerr << "Error: invalid number of channels\n";
             exit(EXIT_FAILURE);
         }
         if (audioFile.samplerate() != 44100) {
             std::cerr << "Error: currently supports only 44100 Hz of sample rate\n";
             exit(EXIT_FAILURE);
         }
-
         std::vector<unsigned char> binarySignature;
         int nFrames = static_cast<int>(audioFile.frames());
-        std::vector<short> samples(nFrames * 2);
+        int nChannels = audioFile.channels();
+
+        std::vector<short> samples(nFrames * nChannels);
         audioFile.readf(samples.data(), nFrames);
 
         auto* in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * ws);
@@ -45,12 +46,16 @@ namespace UTILS {
 
         for (int n = 0; n <= (nFrames - ws * ds) / (sh * ds); ++n) {
             for (int k = 0; k < ws; ++k) {
-                in[k][0] = static_cast<double>(samples[(n * (sh * ds) + k * ds) * 2] +
-                                               samples[((n * (sh * ds) + k * ds) * 2) + 1]);
-                for (int l = 1; l < ds; ++l) {
-                    in[k][0] += static_cast<double>(samples[(n * (sh * ds) + k * ds + l) * 2] +
-                                                    samples[((n * (sh * ds) + k * ds + l) * 2) + 1]);
+                in[k][0] = 0.0;
+                for (int c = 0; c < nChannels; ++c) {
+                    in[k][0] += static_cast<double>(samples[(n * (sh * ds) + k * ds) * nChannels + c]);
                 }
+                for (int l = 1; l < ds; ++l) {
+                    for (int c = 0; c < nChannels; ++c) {
+                        in[k][0] += static_cast<double>(samples[(n * (sh * ds) + k * ds + l) * nChannels + c]);
+                    }
+                }
+                in[k][0] /= (ds * nChannels);  // Averaging across the downsampling factor and number of channels
             }
 
             fftw_execute(plan);
@@ -105,7 +110,6 @@ namespace UTILS {
             SndfileHandle outFile(outputFilePath.c_str(), SFM_WRITE, sfinfo.format, sfinfo.channels, sfinfo.samplerate);
             if (outFile.error()) {
                 std::cerr << "Error writing segment file: " << outputFilePath << std::endl;
-                std::cerr << "Error details: " << sf_strerror(NULL) << std::endl;  // Print the specific libsndfile error message
                 continue;
             }
 
@@ -113,5 +117,59 @@ namespace UTILS {
                 std::cerr << "Error writing audio data to segment file: " << outputFilePath << std::endl;
             }
         }
+    }
+
+    std::vector<unsigned char> computeFFTSegmentSignature(const std::vector<short>& samples, int sampleRate, int channels, int ws, int sh, int ds, int nf) {
+        if (sampleRate != 44100) {
+            std::cerr << "Error: currently supports only 44100 Hz of sample rate\n";
+            exit(EXIT_FAILURE);
+        }
+
+        int nFrames = static_cast<int>(samples.size()) / channels;
+        std::vector<unsigned char> binarySignature;
+
+        auto* in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * ws);
+        auto* out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * ws);
+        fftw_plan plan = fftw_plan_dft_1d(ws, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+        std::vector<double> power(ws / 2);
+        std::vector<unsigned> maxPowerIdx(ws / 2);
+
+        for (int n = 0; n <= (nFrames - ws * ds) / (sh * ds); ++n) {
+            for (int k = 0; k < ws; ++k) {
+                in[k][0] = 0.0;
+                for (int c = 0; c < channels; ++c) {
+                    in[k][0] += static_cast<double>(samples[(n * (sh * ds) + k * ds) * channels + c]);
+                }
+                for (int l = 1; l < ds; ++l) {
+                    for (int c = 0; c < channels; ++c) {
+                        in[k][0] += static_cast<double>(samples[(n * (sh * ds) + k * ds + l) * channels + c]);
+                    }
+                }
+                in[k][0] /= (ds * channels);  // Averaging across the downsampling factor and number of channels
+            }
+
+            fftw_execute(plan);
+
+            for (int k = 0; k < static_cast<int>(power.size()); ++k) {
+                power[k] = out[k][0] * out[k][0] + out[k][1] * out[k][1];
+            }
+
+            std::iota(maxPowerIdx.begin(), maxPowerIdx.end(), 0);
+            std::partial_sort(maxPowerIdx.begin(), maxPowerIdx.begin() + nf, maxPowerIdx.end(), [&power](int i, int j) {
+                return power[i] > power[j];
+            });
+
+            for (int i = 0; i < nf; ++i) {
+                auto c = maxPowerIdx[i] > 255 ? 255 : maxPowerIdx[i];
+                binarySignature.push_back(static_cast<unsigned char>(c));
+            }
+        }
+
+        fftw_destroy_plan(plan);
+        fftw_free(in);
+        fftw_free(out);
+
+        return binarySignature;
     }
 } // UTILS
