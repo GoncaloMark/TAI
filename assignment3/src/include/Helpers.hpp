bf2fac774f21ac6c04c07effbea045dc6e7cf220
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2024-06-01 15:39:06 +0100

    Update Helpers.hpp

--- assignment3/src/include/Helpers.hpp
+++ assignment3/src/include/Helpers.hpp
@@ -19,27 +19,30 @@
 
 namespace UTILS {
 
-        // Function to parse integer arguments safely
-        int parseInt(const char* arg);
+    // Function to parse integer arguments safely
+    int parseInt(const char* arg);
 
-        /**
-        * Function to compute the FFT signature of an audio file.
-        *
-        * This function processes a stereo audio file, converting it to mono and then
-        * computing the FFT of overlapping windows of the audio signal. It identifies
-        * the most significant frequencies in each window and stores their indices in
-        * a binary format.
-        *
-        * @param audioFile A reference to the Snd file Handle object representing the audio file.
-        * @param ws The size of the window for computing the FFT (default is 1024).
-        * @param sh The window overlap (default is 256).
-        * @param ds The down-sampling factor (default is 4).
-        * @param nf The number of significant frequencies to identify (default is 4).
-        * @return A vector of unsigned char containing the binary signature of the audio file.
-        */
-        std::vector<unsigned char> computeFFTSignature(SndfileHandle& audioFile, int ws, int sh, int ds, int nf);
+    /**
+    * Function to compute the FFT signature of an audio file.
+    *
+    * This function processes a stereo audio file, converting it to mono and then
+    * computing the FFT of overlapping windows of the audio signal. It identifies
+    * the most significant frequencies in each window and stores their indices in
+    * a binary format.
+    *
+    * @param audioFile A reference to the Snd file Handle object representing the audio file.
+    * @param ws The size of the window for computing the FFT (default is 1024).
+    * @param sh The window overlap (default is 256).
+    * @param ds The down-sampling factor (default is 4).
+    * @param nf The number of significant frequencies to identify (default is 4).
+    * @return A vector of unsigned char containing the binary signature of the audio file.
+    */
+    std::vector<unsigned char> computeFFTSignature(SndfileHandle& audioFile, int ws, int sh, int ds, int nf);
 
-        void createSegments(const std::string& inputFilePath, const std::string& outputDir, int segmentDuration);
+    void createSegments(const std::string& inputFilePath, const std::string& outputDir, int segmentDuration);
+
+    std::vector<unsigned char> computeFFTSegmentSignature(const std::vector<short>& samples, int sampleRate, int channels, int ws, int sh, int ds, int nf);
+
 
 } // UTILS
 
